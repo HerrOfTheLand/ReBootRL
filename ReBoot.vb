8ffa21175d484088b4e5aa91845c00119873fce1
Baron Dominic Lexertux <78223678+HerrOfTheLand@users.noreply.github.com>
2021-04-03 20:35:51 +0300

    Basic map

--- ReBoot.vb
+++ ReBoot.vb
@@ -1,27 +1,59 @@
 Module ReBoot
+    Enum TerrarianType
+        None
+        Ground
+        Wall
+    End Enum
+    Structure Entity
+        Dim Glyph As Char
+    End Structure
+    Structure Cell
+        Dim Terrarian As TerrarianType
+        Dim Entity As Entity
+    End Structure
     Sub ProcessText(ParamArray strings As String())
         Console.WriteLine("+--------------------------------")
         For i = 0 To strings.Length - 1
             Console.WriteLine($"| {strings(i)}")
         Next
     End Sub
-    Sub ProcessMap(playerX As Integer, playerY As Integer)
+    Sub ProcessMap(cameraX As Integer, cameraY As Integer, map As Cell(,))
+        Dim screen(8, 8) As Char
         Console.WriteLine("+--------------------------------")
-        Console.WriteLine("| +-----+")
-        Console.WriteLine("| |.....|")
-        Console.WriteLine("| |.....|")
-        Console.WriteLine("| |.....|")
-        Console.WriteLine("| |.....|")
-        Console.WriteLine("| |.....|")
-        Console.WriteLine("| +-----+")
-        Console.CursorLeft += 2 + playerX
-        Console.CursorTop -= 7 - playerY
-        Console.Write("@")
-        Console.CursorLeft -= 2 + playerX + 1
-        Console.CursorTop += 7 - playerY
+        Console.WriteLine("| +---------+")
+        For y = cameraY To cameraY + 8
+            For x = cameraX To cameraX + 8
+
+            Next
+        Next
+            For y = cameraY To cameraY + 8
+            Console.Write("| |")
+            For x = cameraX To cameraX + 8
+                If x < 0 OrElse y < 0 OrElse x > map.GetUpperBound(1) OrElse y > map.GetUpperBound(0) Then
+                    Console.Write("X")
+                    Continue For
+                End If
+                If map(y, x).Entity.Glyph <> Chr(0) Then
+                    Console.Write(map(y, x).Entity.Glyph)
+                    Continue For
+                End If
+                Select Case map(y, x).Terrarian
+                    Case TerrarianType.None
+                        Console.Write(" ")
+                    Case TerrarianType.Ground
+                        Console.Write(".")
+                    Case TerrarianType.Wall
+                        Console.Write("#")
+                    Case Else
+                        Console.Write("?")
+                End Select
+            Next
+            Console.WriteLine("|")
+        Next
+        Console.WriteLine("| +---------+")
     End Sub
     Function ProcessStringInput(title As String) As String
-        Console.WriteLine("+--------------------------------")
+        Console.WriteLine($"+--------------------------------")
         Console.WriteLine($"| {title}:")
         Do
             Console.Write("| >>> ")
@@ -34,7 +66,7 @@ Module ReBoot
         Return Console.ReadLine()
     End Function
     Function ProcessDialog(title As String, ParamArray entries As String()) As Integer
-        Console.WriteLine("+--------------------------------")
+        Console.WriteLine($"+--------------------------------")
         Console.WriteLine($"| {title}:")
         For i = 0 To entries.Length - 1
             Console.WriteLine($"| {i + 1}) {entries(i)}")
@@ -93,62 +125,95 @@ Module ReBoot
                 Case 2
                     Dim playerX As Integer = Integer.Parse(savedGame.<character>.<position>.<x>.Value)
                     Dim playerY As Integer = Integer.Parse(savedGame.<character>.<position>.<y>.Value)
+                    Dim map(3, 3) As Cell
+                    map(1, 1).Terrarian = TerrarianType.Wall
+                    map(playerY, playerX).Entity.Glyph = "@"c
                     Do
                         Select Case ProcessStringInput("Command [type help for help]").ToLower()
                             Case "help"
                                 ProcessText("Command      | Result",
                                             "-------------+--------",
                                             "help         | Display this help",
-                                            "map          | Display local map",
-                                            "quit         | Quit to main menu",
+                                            "quit         | Quit to main menu (game will be lost)",
+                                            "save         | Save game",
+                                            "look         | Display local map",
+                                            "look up      | Display local map towards up",
+                                            "look down    | Display local map towards down",
+                                            "look left    | Display local map towards left",
+                                            "look right   | Display local map towards right",
                                             "[move] up    | Step up",
                                             "[move] down  | Step down",
                                             "[move] left  | Step left",
                                             "[move] right | Step right")
                                 Continue Do
-                            Case "map"
-                                ProcessMap(playerX, playerY)
+                            Case "look"
+                                ProcessMap(playerX - 4, playerY - 4, map)
+                                Continue Do
+                            Case "look up"
+                                ProcessMap(playerX - 4, playerY - 8, map)
+                                Continue Do
+                            Case "look down"
+                                ProcessMap(playerX - 4, playerY, map)
+                                Continue Do
+                            Case "look left"
+                                ProcessMap(playerX - 8, playerY - 4, map)
+                                Continue Do
+                            Case "look right"
+                                ProcessMap(playerX, playerY - 4, map)
                                 Continue Do
                             Case "move up", "up"
-                                If playerY - 1 < 1 Then
+                                If playerY < 1 OrElse map(playerY - 1, playerX).Terrarian = TerrarianType.Wall Then
                                     ProcessText("Blocked...")
                                     Continue Do
                                 Else
+                                    map(playerY, playerX).Entity.Glyph = Chr(0)
                                     playerY -= 1
+                                    map(playerY, playerX).Entity.Glyph = "@"c
                                     savedGame.<character>.<position>.<y>.Value = playerY.ToString()
                                     ProcessText("You step up")
                                 End If
                             Case "move down", "down"
-                                If playerY + 1 > 5 Then
+                                If playerY > map.GetUpperBound(0) - 1 OrElse map(playerY + 1, playerX).Terrarian = TerrarianType.Wall Then
                                     ProcessText("Blocked...")
                                     Continue Do
                                 Else
+                                    map(playerY, playerX).Entity.Glyph = Chr(0)
                                     playerY += 1
+                                    map(playerY, playerX).Entity.Glyph = "@"c
                                     savedGame.<character>.<position>.<y>.Value = playerY.ToString()
                                     ProcessText("You step down")
                                 End If
                             Case "move left", "left"
-                                If playerX - 1 < 1 Then
+                                If playerX < 1 OrElse map(playerY, playerX - 1).Terrarian = TerrarianType.Wall Then
                                     ProcessText("Blocked...")
                                     Continue Do
                                 Else
+                                    map(playerY, playerX).Entity.Glyph = Chr(0)
                                     playerX -= 1
+                                    map(playerY, playerX).Entity.Glyph = "@"c
                                     savedGame.<character>.<position>.<x>.Value = playerX.ToString()
                                     ProcessText("You step left")
                                 End If
                             Case "move right", "right"
-                                If playerX + 1 > 5 Then
+                                If playerX > map.GetUpperBound(1) - 1 OrElse map(playerY, playerX + 1).Terrarian = TerrarianType.Wall Then
                                     ProcessText("Blocked...")
                                     Continue Do
                                 Else
+                                    map(playerY, playerX).Entity.Glyph = Chr(0)
                                     playerX += 1
+                                    map(playerY, playerX).Entity.Glyph = "@"c
                                     savedGame.<character>.<position>.<x>.Value = playerX.ToString()
                                     ProcessText("You step right")
                                 End If
+                            Case "save"
+                                ProcessText("Saving the game...")
+                                savedGame.Save("save.xml")
+                                Continue Do
                             Case "quit"
                                 ProcessText("Quiting to main menu...")
-                                savedGame.Save("save.xml")
                                 Exit Do
+                            Case "d"
+                                ProcessText("Debug", $"X: {playerX}", $"Y: {playerY}")
                             Case Else
                                 ProcessText("Unknown command")
                                 Continue Do
@@ -157,12 +222,13 @@ Module ReBoot
                     Loop
                 Case 3
                     ProcessText("ReBoot, Text-based technofantasy roguelike.",
-                                "Version 0.0.2 'The Cretion art'", ' next 'The Second day'
+                                "Version 0.0.3 'The Cretion art'", ' next 'The Second day'
                                 "-------------------------------",
                                 "Interaction:",
                                 "  Enter comand to execute it",
                                 "  Enter dialogue option or its number to choose it",
                                 "  Commands and dialogue opinions are case-insesitive",
+                                "  Type 'help' to get help",
                                 "Story:",
                                 "  At the beginning there were humans, demons, and angels.",
                                 "  Demons and angels were in the holy war, and the battleground was a human world, the Gaia.",
